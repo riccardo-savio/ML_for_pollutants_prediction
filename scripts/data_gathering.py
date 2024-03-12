5802cae87da782839c1e2685c6a096b233819cdd
Riccardo Savio <riccardosaviomail@gmail.com>
2024-03-12 20:12:22 +0100

    Add data gathering and analysis scripts

--- scripts/data_gathering.py
+++ scripts/data_gathering.py
@@ -1,52 +1,65 @@
 import pandas as pd
 from sodapy import Socrata
+import os
+# ["evzn-32bs", "cthp-zqrr", "nr8w-tj77", "g2hp-ar79"]
 
+time_periods = {
+    "evzn-32bs": "1968-1999",
+    "cthp-zqrr": "2000-2009",
+    "nr8w-tj77": "2010-2017",
+    "g2hp-ar79": "2018-2022",
+}
 
-def export_data(sensors_dat_results, sensors_df, sensor_id):
-    sdata_1_df = pd.DataFrame.from_records(sensors_dat_results)
-    merged_sensor_data = pd.merge(sensors_df, sdata_1_df, on="idsensore", how="right")
-    merged_sensor_data.filter(
-        items=[
-            "idsensore",
-            "nometiposensore",
-            "nomestazione",
-            "provincia",
-            "comune",
-            "data",
-            "valore",
-            "unitamisura",
-            "stato",
-        ]
-    ).to_csv(f"data/sensors/{sensor_id}.csv", index=False, mode="a", header=False)
-
-
-def get_dati_lombardia():
-    sensors = Socrata("www.dati.lombardia.it", app_token="HgtqW8PtAIt17vyGLqsGoRyHx")
-    sensors_results = sensors.get("ib47-atvt", limit=1000)
-    sensors_df = pd.DataFrame.from_records(sensors_results)
-    sensors_df.to_csv(path_or_buf="data/sensors.csv", index=False, mode="w")
-
-    rslt_df = sensors_df[
-        sensors_df["nometiposensore"].isin(
-            ["PM10 (SM2005)", "Particelle sospese PM2.5", "Particolato Totale Sospeso"]
-        )
-    ]
-    rslt_df.to_csv("data/sensors_f.csv", index=False, mode="w")
-
-    # cycle through the sensors and get the data
-    for time_period in ["nicp-bhqi"]:
-        for index, row in rslt_df.iterrows():
-            sensor_id = row["idsensore"]
+def get_sensor_data(sensor_id: str, time_period: str, nometiposensore: str,  update: bool = False ):
+    if update:
+        try:
             sensor_data = Socrata(
                 "www.dati.lombardia.it", app_token="HgtqW8PtAIt17vyGLqsGoRyHx"
             )
-            sensors_dat_results = sensor_data.get(
-                time_period, limit=1000000, idsensore=sensor_id, stato="VA"
+            sensors_data_results = sensor_data.get(
+                    time_period, limit=1000000, idsensore=sensor_id, stato="VA"
+                )
+            sensor_data_df = pd.DataFrame.from_records(sensors_data_results)
+            
+            if not os.path.exists(f"data/sensors/{time_periods[time_period]}/{nometiposensore}/"):
+                os.makedirs(f"data/sensors/{time_periods[time_period]}/{nometiposensore}/")
+                
+            sensor_data_df.to_csv(
+                f"data/sensors/{time_periods[time_period]}/{nometiposensore}/{sensor_id}.csv",
+                index=False,
+                mode="w",
             )
+        except:
+            sensor_data_df = pd.DataFrame()
+            
+        return sensor_data_df
+    else:
+        try:
+            return pd.read_csv(f"data/sensors/{time_periods[time_period]}/{nometiposensore}/{sensor_id}.csv")
+        except:
+            return pd.DataFrame()
 
-            if sensors_dat_results:
-                print(f"{sensor_id} {time_period}")
-                export_data(sensors_dat_results, rslt_df, sensor_id)
+def get_ARPA_sensors(update: bool = False):
+    if update:
+        sensors = Socrata(
+            "www.dati.lombardia.it", app_token="HgtqW8PtAIt17vyGLqsGoRyHx"
+        )
+        sensors_results = sensors.get("ib47-atvt", limit=10000)
+        sensors_df = pd.DataFrame.from_records(sensors_results)
+        sensors_df.to_csv("data/sensors.csv", index=False, mode="w")
+        return sensors_df
+    else:
+        return pd.read_csv("data/sensors.csv")
 
 
-get_dati_lombardia()
+def main():
+    sensosr_df = get_ARPA_sensors(update=True)
+    for sensor_id in sensosr_df["idsensore"]:
+        for time_period in time_periods:
+            get_sensor_data(sensor_id, time_period, sensosr_df[sensosr_df["idsensore"] == sensor_id]["nometiposensore"].iloc[0], update=True)
+            print(f"Sensor {sensor_id} for time period {time_periods[time_period]} has been updated")
+
+main()
+
+
+
--- scripts/data_study.py
+++ scripts/data_study.py
@@ -1,29 +1,17 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 from scipy import stats
-import datetime as dt
 
 
-df = pd.read_csv("data/sensors/12574.csv")
-
+df = pd.read_csv("data/sensors/5507.csv")
 
 df["data"] = pd.to_datetime(df["data"])
 df_filtered = df.filter(items=["data", "valore"])
-df_grouped = df_filtered.groupby(df['data'].dt.to_period('D')).mean()
+df_grouped = df_filtered.groupby(df["data"].dt.to_period("D")).mean()
 df_grouped["data"] = pd.to_datetime(df_grouped["data"])
-df_grouped["data"]= df_grouped['data'].map(dt.datetime.toordinal)
 df_grouped.to_csv("data/12574_grouped.csv", index=False, mode="w")
 
-x = df_grouped["data"]
-y = df_grouped["valore"]
 
-slope, intercept, r, p, std_err = stats.linregress(x, y)
-
-def myfunc(x):
-  return slope * x + intercept
-
-mymodel = list(map(myfunc, x))
-
-plt.scatter(x, y)
-plt.plot(x, mymodel, color="r")
+plt.plot(df_grouped["data"], df_grouped["valore"])
+plt.legend([df["nometiposensore"].iloc[0]])
 plt.show()
